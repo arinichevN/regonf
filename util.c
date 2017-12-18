6106e04820c25a9d152e0eae1640393c1f249d78
Arinichev Nikolay <arinichev_n@mail.ru>
2017-12-18 11:45:08 +0300

    c

--- util.c
+++ util.c
@@ -215,21 +215,21 @@ void printData(ACPResponse *response) {
     SEND_STR("+-----------+-----------------------------------------------+-----------------------------------------------+\n")
     SEND_STR("|           |                   EM heater                   |                  EM cooler                    |\n")
     SEND_STR("+           +-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+\n")
-    SEND_STR("|  prog_id  |     id    | remote_id |  pwm_rsl  | peer_link |     id    | remote_id |  pwm_rsl  | peer_link |\n")
+    SEND_STR("|  prog_id  |     id    | remote_id |  pwm_rsl  |  peer_id  |     id    | remote_id |  pwm_rsl  |  peer_id  |\n")
     SEND_STR("+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+\n")
     PROG_LIST_LOOP_ST
-    snprintf(q, sizeof q, "|%11d|%11d|%11d|%11f|%11p|%11d|%11d|%11f|%11p|\n",
+    snprintf(q, sizeof q, "|%11d|%11d|%11d|%11f|%11s|%11d|%11d|%11f|%11s|\n",
             curr->id,
 
             curr->reg.heater.em.id,
             curr->reg.heater.em.remote_id,
             curr->reg.heater.em.pwm_rsl,
-            (void *) curr->reg.heater.em.source,
+             curr->reg.heater.em.source->id,
 
             curr->reg.cooler.em.id,
             curr->reg.cooler.em.remote_id,
             curr->reg.cooler.em.pwm_rsl,
-            (void *) curr->reg.cooler.em.source
+          curr->reg.cooler.em.source->id
             );
     SEND_STR(q)
     PROG_LIST_LOOP_SP
@@ -239,14 +239,14 @@ void printData(ACPResponse *response) {
     SEND_STR("+-----------+-----------+-----------+-----------+------------------------------------------+\n")
     SEND_STR("|           |           |           |           |                   value                  |\n")
     SEND_STR("|           |           |           |           |-----------+-----------+-----------+------+\n")
-    SEND_STR("|    id     |    id     | remote_id | peer_link |   value   |    sec    |   nsec    | state|\n")
+    SEND_STR("|    id     |    id     | remote_id |  peer_id  |   value   |    sec    |   nsec    | state|\n")
     SEND_STR("+-----------+-----------+-----------+-----------+-----------+-----------+-----------+------+\n")
     PROG_LIST_LOOP_ST
-    snprintf(q, sizeof q, "|%11d|%11d|%11d|%11p|%11f|%11ld|%11ld|%6d|\n",
+    snprintf(q, sizeof q, "|%11d|%11d|%11d|%11s|%11f|%11ld|%11ld|%6d|\n",
             curr->id,
             curr->reg.sensor.id,
             curr->reg.sensor.remote_id,
-            (void *) curr->reg.sensor.source,
+           curr->reg.sensor.source->id,
             curr->reg.sensor.value.value,
             curr->reg.sensor.value.tm.tv_sec,
             curr->reg.sensor.value.tm.tv_nsec,
